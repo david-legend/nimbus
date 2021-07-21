312a2ccc4bd5f639e3e88718d0e1f0a1433e3b91
David <dcobbina@bridgeinnovation.io>
2021-07-21 07:17:55 +0000

    added portfolio images

--- lib/values/images.dart
+++ lib/values/images.dart
@@ -6,8 +6,8 @@ class ImagePath {
   static const String screenshotsDir = "assets/screenshots";
 
   //Splash Items
-  static const String LOGO_DARK = "$imageDir/logo-dark.png";
-  static const String LOGO_LIGHT = "$imageDir/logo-light.png";
+  static const String LOGO_DARK = "$imageDir/logo_dark.png";
+  static const String LOGO_LIGHT = "$imageDir/logo_light.png";
   static const String BLOB_BLACK = "$imageDir/blob_black.png";
   static const String DEV_HEADER = "$imageDir/dev_header.png";
   static const String DOTS_GLOBE_GREY = "$imageDir/dots_globe_grey.png";
@@ -16,7 +16,14 @@ class ImagePath {
   static const String DEV_ABOUT_ME = "$imageDir/dev_aboutme.png";
   static const String DEV_AWARD = "$imageDir/dev_award.png";
   static const String GHANA_FLAG = "$imageDir/ghana_flag.png";
-  static const String BLOG_01 = "$imageDir/blog_01-700x700.jpeg";
-  static const String BLOG_02 = "$imageDir/blog_02-700x700.jpeg";
-  static const String BLOG_03 = "$imageDir/blog_03-700x700.jpeg";
+  static const String BLOG_01 = "$imageDir/blog_01.jpg";
+  static const String BLOG_02 = "$imageDir/blog_02.jpg";
+  static const String BLOG_03 = "$imageDir/blog_03.jpg";
+  static const String ICON_BOX = "$imageDir/icon_box.jpg";
+  static const String PORTFOLIO_1 = "$imageDir/protfolio_01.jpg";
+  static const String PORTFOLIO_2 = "$imageDir/protfolio_02.jpg";
+  static const String PORTFOLIO_3 = "$imageDir/protfolio_03.jpg";
+  static const String PORTFOLIO_4 = "$imageDir/protfolio_04.jpg";
+  static const String PORTFOLIO_5 = "$imageDir/protfolio_05.jpg";
+  static const String PORTFOLIO_6 = "$imageDir/protfolio_06.jpg";
 }
